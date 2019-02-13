d41bd0d7997a7d2b21ccdf96b4c09f90ea43951a
huyN1349 <huy.nguyen1349@gmail.com>
2019-02-13 00:01:50 -0800

    Added rolling dice animation for DICE Project.

--- DICE/index.js
+++ DICE/index.js
@@ -1,6 +1,9 @@
-var randomNumber1 = Math.floor(Math.random()*6 + 1);
-var randomNumber2 = Math.floor(Math.random()*6 + 1);
+var randomNumber1 = Math.floor(Math.random() * 6 + 1);
+var randomNumber2 = Math.floor(Math.random() * 6 + 1);
 var resultDisplay = "";
+var n = 0;
+var numberAnimation1;
+var numberAnimation2;
 
 if (randomNumber1 > randomNumber2) {
   resultDisplay = "Player 1 Wins!";
@@ -10,6 +13,17 @@ if (randomNumber1 > randomNumber2) {
   resultDisplay = "Draw!";
 }
 
-document.querySelector(".img1").setAttribute("src",`images/dice${randomNumber1}.png`);
-document.querySelector(".img2").setAttribute("src",`images/dice${randomNumber2}.png`);
-document.querySelector("h1").innerHTML = resultDisplay;
+
+var interval = setInterval(function() {
+  numberAnimation1 = Math.floor(Math.random() * 6 + 1);
+  numberAnimation2 = Math.floor(Math.random() * 6 + 1);
+  document.querySelector(".img1").setAttribute("src", `images/dice${numberAnimation1}.png`);
+  document.querySelector(".img2").setAttribute("src", `images/dice${numberAnimation2}.png`);
+  n++;
+  if (n == 10) {
+    clearInterval(interval);
+    document.querySelector(".img1").setAttribute("src", `images/dice${randomNumber1}.png`);
+    document.querySelector(".img2").setAttribute("src", `images/dice${randomNumber2}.png`);
+    document.querySelector("h1").innerHTML = resultDisplay;
+  }
+}, 100)
